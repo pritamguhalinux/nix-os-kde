336cd2aa68aebffcef9a42cedbdae4c1bbec298c
Pritam Linux <110685901+pritamguhalinux@users.noreply.github.com>
2026-09-25 20:17:06 +0530

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,25 +1,21 @@
 # Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
 { config, pkgs, ... }:
 
 {
- boot.kernelPackages = pkgs.linuxPackages_latest;
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      #inputs.home-manager.nixosModules.default
     ];
 
-  # Bootloader.
+  # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.plymouth.enable = true; # for plymouth logo
-
-
-  programs.kdeconnect.enable = true;
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -54,13 +50,24 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-  services.sssd.kcm = true;
-  #services.displayManager.sddm.theme = "sddm-suger-dark";
+  #services.displayManager.sddm.enable = true;
 
-  nix.settings.experimental-features = ["nix-command" "flakes"];
- # nix.settings.experimental-features = nix-command flakes;
+    services.displayManager.sddm = {
+    enable = true;
+    extraPackages = with pkgs; [
+      sddm-astronaut
+      sddm-sugar-dark
+     ];
+
+    #theme = "sddm-astronaut-theme";
+    #settings = {
+     # Theme = {
+      #  Current = "sddm-astronaut-theme";
+      #};
+    };
+  #};
+
+  services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -68,11 +75,52 @@
     variant = "";
   };
 
+  #program BASH enable
+
+
+ programs.bash = {
+  enable = true;
+  interactiveShellInit = ''
+  hyfetch
+    eval "$(starship init bash)"
+    alias ll='ls -la'
+    export PATH="$HOME/bin:$PATH"
+  '';
+  shellAliases = {
+    ll = "ls -la";
+    la = "ls -A";
+  };
+};
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
+  #Flatpak Service Enable
+  services.flatpak.enable = true;
+
+  #Bluetuth enable
+  hardware.bluetooth = {
+  enable = true;
+  powerOnBoot = true;
+  settings = {
+    General = {
+      Experimental = true;
+      FastConnectable = true;
+    };
+    Policy = {
+      AutoEnable = true;
+    };
+  };
+};
+
+  #Program KDEConnect Enable.
+   programs.kdeconnect = {
+    enable = true;
+    #package = pkgs.gnomeExtensions.gsconnect;
+  };
+
+
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -80,18 +128,14 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    # jack.enable = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  # services.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.pritam = {
+  users.users."pritam" = {
     isNormalUser = true;
     description = "Pritam";
     extraGroups = [ "networkmanager" "wheel" ];
@@ -101,109 +145,96 @@
     ];
   };
 
-  # font
-  fonts.fontDir.enable = true;
-
   # Install firefox.
   programs.firefox.enable = true;
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
-    micro
-    vivaldi
-   # protonmail-bridge
-    #protonmail-bridge-gui
-    telegram-desktop
-    signal-desktop
-    freetube
-    git
-    rustc
-    rustup-toolchain-install-master
-    rustus
-    cargo
-    rustfmt
-    clippy
-    ruby
-    bundler
-    rustup
-    wineWowPackages.stable
-    winetricks
-    blender
-   # krita
-    shotcut
-    obs-studio
-    vlc
-   spotify
-   # amberol
-    gimp
-   # libtiff-4.0.3-opentoon
-    synfigstudio
-    glava
-    cava
-    #rhythmbox
-    #opentoonz
-
-    # Utilities
-    weston
-    wget
-    hblock
-    parabolic
-    heimdall
-    ventoy-full
-    pkgs.android-tools
-    adb-sync
-    wpsoffice
-    hyfetch
-    neofetch
-    anydesk
-    klavaro
-    tuxtype
-    appimage-run
-    htop
-    fish
-    gparted
-    arch-install-scripts
-    podman
-
-    #KDE
-    libsForQt5.qtstyleplugin-kvantum
-    kdePackages.qtstyleplugin-kvantum
-    libsForQt5.partitionmanager
-    haruna
-    sddm-sugar-dark
-    #ibsForQt5.sddm-kcm
-    kdePackages.sddm-kcm
-    #kdePackages.kamoso
-
-    # Font all
-    noto-fonts # Broad Unicode coverage for many languages
-        #noto-fonts-cjk # Chinese, Japanese, Korean
-       
-  ];
-
-
-  virtualisation.libvirtd.enable = true;
-  programs.virt-manager.enable = true;
-
-  #virtualisation.waydroid.enable = true;
-
-  programs.bash = {
-    interactiveShellInit = ''
-      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-      then
-        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
-      fi
-    '';
-  };
-
+  # List packages installed in system profile.
+  # You can use https://search.nixos.org/ to find more packages (and options).
+   environment.systemPackages = with pkgs; [
+     #vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    # wget
+     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  wget															#Download Online
+  micro															#Terminal Esey Text Editor
+  neovim														#Text Editor
+  #gnuchess
+  #blender
+  # opera														#Chess Game
+  firefox-bin													#Firefox Perfect
+  xonotic
+ # steam-run														#Single Person Shooting Game
+  #assaultcube
+  #gnome-browser-connector										#Connect gnome With Browser(Firefox)
+  #gnomeExtensions.gsconnect										#Connecting Android Mobile With Gnome Desktop
+  #gnomeExtensions.blur-my-shell									#Gnome Extention for Blur Effect
+  #gnomeExtensions.dash-to-panel									#For Dash To Pannel
+  #gnomeExtensions.compiz-windows-effect							#For wabbily Windows
+  #gnomeExtensions.coverflow-alt-tab
+  hblock							#for Coverflow Windows
+  gnome-boxes
+  #gnome-mahjongg
+  #gnome-chess
+  papirus-icon-theme
+  spotify
+  parabolic
+  heimdall
+  git
+  glava
+  starship
+  #ventoy-full
+  #warp-terminal
+  rustc
+  rustup-toolchain-install-master
+  gnuchess
+  rustus
+  cargo
+  rustfmt
+  clippy
+  ruby
+  bundler
+  rustup
+  pkgs.android-tools
+  adb-sync
+  #waydroid
+  freetube
+  signal-desktop
+  wpsoffice
+  hyfetch
+  honeyfetch
+  #neofetch
+  anydesk
+  #vscode
+  vscodium
+  #obs-studio
+  #amberol
+  klavaro
+  tuxtype
+  krita
+  shotcut
+  #google-chrome
+  #brave
+  obs-studio
+  appimage-run
+  htop
+  vlc
+ # steam
+  #whitesur-icon-theme
+  #bibata-cursors
+  telegram-desktop
+  gimp
+  #ventoy
+  #gnomeExtensions.gsconnect
+  fish
+  gparted
+  kdePackages.sddm-kcm
+  kdePackages.qtstyleplugin-kvantum
+  sddm-astronaut
+   sddm-sugar-dark
+   kdePackages.qtmultimedia
+   ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -216,7 +247,15 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+   services.openssh.enable = true;
+
+
+  virtualisation.libvirtd.enable = true;
+    programs.virt-manager.enable = true;
+
+
+  # Nix settings
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
@@ -224,12 +263,28 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "26.05"; # Did you read the comment?
 
 }
